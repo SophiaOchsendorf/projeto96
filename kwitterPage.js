2295382dfc18f8c626f760bce4bc159e3094e901
SophiaOchsendorf <105024506+SophiaOchsendorf@users.noreply.github.com>
2022-08-04 20:49:06 -0300

    Add files via upload

---
+++ kwitterPage.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDj2oa_wWa7FGwUQyEMBEuHIxpPVo1nWds",
+    authDomain: "vamosconversar-c60ee.firebaseapp.com",
+    databaseURL: "https://vamosconversar-c60ee-default-rtdb.firebaseio.com",
+    projectId: "vamosconversar-c60ee",
+    storageBucket: "vamosconversar-c60ee.appspot.com",
+    messagingSenderId: "746989659554",
+    appId: "1:746989659554:web:dc5f57f9860143ac587efb"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
